e4d8f3fda584599ccd64912a7ad68fe4ddc9668c
LeeJiwooKor <njbsandrewlee@gmail.com>
2025-11-20 23:12:36 +0900

    added data.py

--- data.py
+++ data.py
@@ -1,3 +1,7 @@
+#exapmle data generation script for generating training data
+# generates 200 data points where the input is two random numbers between 0 and 1
+# the output is 1 if the numbers are equal, else 0
+
 import random
 import json
 count = 200
@@ -7,7 +11,6 @@ for _ in range(count):
     a = round(random.random(), 2)
     b = round(random.random(), 2)
 
-    # your rule: 1 only if values are exactly the same
     output = 1 if a == b else 0
 
     data = {
